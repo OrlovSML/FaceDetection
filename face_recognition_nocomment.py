1f8c31b2075aa8af1369b79a68908d197c1158fe
Sergey Orlov <icqcoder@gmail.com>
2022-04-21 17:16:15 +0300

    Update face_recognition_nocomment.py

--- face_recognition_nocomment.py
+++ face_recognition_nocomment.py
@@ -1,13 +1,8 @@
 # Face Recognition
-
-# Importing the libraries
 import cv2
 
-# Loading the cascades
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
-# Defining a function that will do the detections
 def detect(gray, frame):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
@@ -19,7 +14,6 @@ def detect(gray, frame):
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
     return frame
 
-# Doing some Face Recognition with the webcam
 video_capture = cv2.VideoCapture(0)
 while True:
     _, frame = video_capture.read()
